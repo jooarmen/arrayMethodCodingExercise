a50cc5072f9529f407cd88bc55ef4d7882c60d85
jooarmen <126363143+jooarmen@users.noreply.github.com>
2023-05-08 19:41:05 -0700

    updated

--- easy.js
+++ easy.js
@@ -1,8 +1,9 @@
-// Find Method:
+// Find Method: 
 // Challenge: Find the first even number in an array.
 
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
+let newNumbers = numbers.find(number => number % 2 === 0);
+console.log(newNumbers);
 // Starter code - Add your solution here
 
 
@@ -10,7 +11,8 @@ var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // Challenge: Sort an array of numbers in ascending order.
 
 var unsortedArray = [5, 3, 8, 1, 9, 2, 7, 4, 6];
-
+let sortedArray = unsortedArray.sort()
+console.log(sortedArray)
 // Starter code - Add your solution here
 
 
@@ -18,7 +20,8 @@ var unsortedArray = [5, 3, 8, 1, 9, 2, 7, 4, 6];
 // Challenge: Filter out all the strings from an array of mixed data types.
 
 var mixedArray = [1, 'apple', 2, 'banana', 3, 'cherry', 4, 'date'];
-
+let filteredArray = mixedArray.filter(element => typeof element !== "string")
+console.log(filteredArray)
 // Starter code - Add your solution here
 
 
